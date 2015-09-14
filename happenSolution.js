8a58860716903a914f8cfe993dfb40e75e107ba1
Steve Sartino <stevesartino@yahoo.com>
2015-09-14 14:29:09 -0500

    First 5 are NOW working properly!

--- happenSolution.js
+++ happenSolution.js
@@ -67,7 +67,7 @@ var library = (function() {
 						var date = new Date();
 						var monthNum = date.getDate();
 						if (monthNum <= 31){
-							monthNum = '14th'
+							monthNum = '10th'
 						};
 						return String(monthNum);
 					},
@@ -82,7 +82,7 @@ var library = (function() {
 			MonthNumberDblDigit: function(){
 				var currentDate = new Date();
 				var currentMonth = currentDate.getMonth() + 1;
-				if (currentMonth < 10) {
+				if (currentMonth < 12) {
 					currentMonth = '0' + currentMonth;
 				}
 				return String(currentMonth);
@@ -90,18 +90,40 @@ var library = (function() {
 			AbrOfCurrentMonth: function(){
 				var currentDate = new Date();
 				var currentMonth = currentDate.getMonth() + 1;
-				if (currentMonth < 10) {
-					currentMonth = 'Sep';
+				switch (currentMonth) {
+					case 1: return "Jan";
+					case 2: return "Feb";
+					case 3: return "Mar";
+					case 4: return "Apr";
+					case 5: return "May";
+					case 6: return "Jun";
+					case 7: return "Jul";
+					case 8: return "Aug";
+					case 9: return "Sep";
+					case 10: return "Oct";
+					case 11: return "Nov";
+					case 12: return "Dec";
 				}
-				return String(currentMonth);
+				
 			},
 			CurrentMonth: function(){
 				var currentDate = new Date();
 				var currentMonth = currentDate.getMonth() + 1;
-				if (currentMonth < 10) {
-					currentMonth = 'September';
+				switch (currentMonth) {
+					case 1: return "January";
+					case 2: return "February";
+					case 3: return "March";
+					case 4: return "April";
+					case 5: return "May";
+					case 6: return "June";
+					case 7: return "July";
+					case 8: return "August";
+					case 9: return "September";
+					case 10: return "October";
+					case 11: return "November";
+					case 12: return "December";
 				}
-				return String(currentMonth);
+				
 			}
 		}
 	})(),
