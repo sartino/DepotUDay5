6d71dffbed8ea32203a0708a9143757714eb04ff
Steve Sartino <stevesartino@yahoo.com>
2015-09-15 08:17:01 -0500

    29 of 30 are working

--- happenSolution.js
+++ happenSolution.js
@@ -17,8 +17,52 @@ var library = (function() {
 	  return {
 		Time: (function() {
 		  return {
-	  	    WithSeconds: function(){},
-	   	    WithOutSeconds: function() {}
+	  	    WithSeconds: function(){
+				function addZero (i) {
+				if (i < 10) {
+					i = "0" + i;
+				} return i;
+				}
+				//var d = new Date();
+				//return String(n);
+				var a_p = "";
+				var d = new Date();
+				var n = addZero(d.getSeconds());
+				var curr_hour = d.getHours();
+					if (curr_hour < 12)
+   						{a_p = "AM";}
+					else {a_p = "PM";}
+					if (curr_hour == 0) {
+   						curr_hour = 12;
+   					} if (curr_hour > 12) {
+   						curr_hour = curr_hour - 12;
+   					}
+					var curr_min = d.getMinutes();
+						curr_min = curr_min + "";
+					if (curr_min.length == 1)
+   						{curr_min = "0" + curr_min;
+   					}
+					return String(curr_hour + ":" + curr_min + ":" + n + " " + a_p);
+			  },
+	   	    WithOutSeconds: function() {
+				var a_p = "";
+				var d = new Date();
+				var curr_hour = d.getHours();
+					if (curr_hour < 12)
+   						{a_p = "AM";}
+					else {a_p = "PM";}
+					if (curr_hour == 0) {
+   						curr_hour = 12;
+   					} if (curr_hour > 12) {
+   						curr_hour = curr_hour - 12;
+   					}
+					var curr_min = d.getMinutes();
+						curr_min = curr_min + "";
+					if (curr_min.length == 1)
+   						{curr_min = "0" + curr_min;
+   					}
+					return String(curr_hour + ":" + curr_min + " " + a_p);
+			   }
 		  }
 		})(),
 		MDY: (function(){
